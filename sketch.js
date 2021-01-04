e7e7cabc71a1a0af68b01868e50863400d6d600e
arnav2811 <73223536+arnav2811@users.noreply.github.com>
2021-01-04 16:37:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,7 +9,8 @@ var drop1, umbrella1
 var maxDrops = 200;
  var ThunderImage
 function preload(){
-    ThunderImage = loadImage("images/thunderbolt/1.png")
+    ThunderImage = loadAnimation("images/thunderbolt/1.png", "images/thunderbolt/2.png", 
+    "images/thunderbolt/3.png", "images/thunderbolt/4.png")
     drop = [
 
     ]
@@ -24,8 +25,9 @@ function setup(){
     drop.push(new Drops(Math.round(random(0, 490)),Math.round(random(0, 200))))
   }
    umbrella1 = new Umbrella()
-   thunder = createSprite(300, 300, 20, 20)
-   thunder.addImage(ThunderImage)
+   thunder = createSprite(200, 120, 20, 20)
+   thunder.addAnimation("running",ThunderImage)
+   thunder.scale = 0.3
 }
 
 function draw(){
@@ -36,5 +38,6 @@ function draw(){
     drop[i].update()
     }
    umbrella1.display()
+   drawSprites()
    }   
 
